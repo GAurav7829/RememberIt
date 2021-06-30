3abace70e1e64e6b94af243aea4076c0437014c7
Gaurav Singh <grvsgh7@gmail.com>
2021-06-30 22:00:50 +0530

    first commit

--- App.js
+++ App.js
@@ -1,14 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import { MaterialIcons } from '@expo/vector-icons';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <TouchableOpacity onPress={() => { }}
+          style={styles.fab}
+        >
+          <MaterialIcons name="add" size={30} color="black" />
+        </TouchableOpacity>
+
+        <StatusBar style="auto" />
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
@@ -18,4 +26,22 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  fab: {
+    backgroundColor: '#F0E71D',
+    borderRadius: 20,
+    width: 60,
+    height: 60,
+    alignItems: 'center',
+    justifyContent: 'center',
+    shadowColor: '#000000',
+    shadowOpacity: 0.2,
+    shadowRadius: 8,
+    shadowOffset: {
+      height: 2,
+      width: 2
+    },
+    position: 'absolute',
+    right: 40,
+    bottom: 40
+  }
 });
